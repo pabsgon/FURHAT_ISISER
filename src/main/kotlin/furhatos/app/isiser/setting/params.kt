e4470ef9b276f500dfb82c65d05ede9546af8de8
Pablo <pablool@kth.se>
2024-06-10 18:59:13 +0200

    Post-experiment adjustments to set it up back to English.

--- src/main/kotlin/furhatos/app/isiser/setting/params.kt
+++ src/main/kotlin/furhatos/app/isiser/setting/params.kt
@@ -1,6 +1,6 @@
 package furhatos.app.isiser.setting
 const val APP_EXECUTION_MODE = DEV
-const val LANGUAGE = "SV" // [EN|SV] EN default
+const val LANGUAGE = "EN" // [EN|SV] EN default
 const val SOURCEDATA_SPREADSHEETID = "1--UZgR4W01c7Z5yml06KCWw4xm8hRBJLVv-bT63iyzg" // The spreadsheet ID with GoogleApi permission granted
 const val SOURCEDATA_RANGE_ENG = "RAWDATA_FOR_LOADING_ENG!A3:AS300"  //The tab where the data lies.
 const val SOURCEDATA_RANGE_SWE = "RAWDATA_FOR_LOADING_SWE!A3:AS300"  //The tab where the data lies.
@@ -13,7 +13,7 @@ const val ENGAGMENT_DISTANCE = 2.0
 const val ROBOT_SPEECH_RATE_CERTAIN = 1.13
 const val ROBOT_SPEECH_RATE_UNCERTAIN = 0.87
 const val SILENT_MILLISECS_PER_DOT = 150
-const val LOG_ALL_EVENTS = true // If true, all events will be logged. Otherwise, only the GUI, dialog and gesture events will be logged.
+const val LOG_ALL_EVENTS = false // If true, all events will be logged. Otherwise, only the GUI, dialog and gesture events will be logged.
 const val MAX_NUM_BACKCHANNEL_WORDS = 2
 const val MAX_BACKCHANNEL_LENGTH = 4000 //Milliseconds.
 const val MAX_NUM_PROBES_AT_PERSUASION = 1 //If greater than 0, probes will be posed by the robot during persuasion (discussion in disagreement) when the user says "No" with no further explanation.
